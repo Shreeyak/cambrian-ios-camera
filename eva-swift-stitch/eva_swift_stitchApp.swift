2351826d97ce4fcb7b7f1a7277914e2c43626e95
shrek <ss.shrek7@gmail.com>
2026-04-14 16:11:13 +0530

    Initial Commit

---
+++ eva-swift-stitch/eva_swift_stitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  eva_swift_stitchApp.swift
+//  eva-swift-stitch
+//
+//  Created by shrek on 4/14/26.
+//
+
+import SwiftUI
+
+@main
+struct eva_swift_stitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
